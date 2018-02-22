d6c1621bec252e8d039768f395c3a7521e082d62
Ken <khaindk@gmail.com>
2018-02-22 23:41:18 +0700

    [Bash] Add script to install bash config

--- installEnv.sh
+++ installEnv.sh
@@ -13,7 +13,13 @@ VIMRC=.vimrc
 TMUX_DIR=tmux
 TMUX_CONF=.tmux.conf
 
-#vim
+BASH_DIR=bash
+BASH_CONF=.bashrc
+BASH_HIST=.bash_history
+BASH_ALIAS=.bash_aliases
+BASH_PRO=.bash_profile
+
+# VIM
 echo "============>Linking vimrc..."
 #ln -s $BASE_DIR/$VIM_DIR ~/.$VIM_DIR
 if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
@@ -25,9 +31,16 @@ vim +PluginInstall +qall
 echo "============>Source vimrc ... "
 source ~/$VIMRC
 
+# TMUX
 echo "============>Linking tmux config ... "
 ln -s $BASE_DIR/$TMUX_DIR ~/.$TMUX_DIR
 ln -s $BASE_DIR/$TMUX_DIR/$TMUX_CONF ~/$TMUX_CONF
 echo "=============>Source tmux.conf ..."
 tmux source-file ~/$TMUX_CONF
 
+# BASH
+echo "============>Linking bash config ... "
+ln -s $BASE_DIR/$BASH_DIR/$BASH_CONF ~/$BASH_CONF
+ln -s $BASE_DIR/$BASH_DIR/$BASH_HIST ~/$BASH_HIST
+ln -s $BASE_DIR/$BASH_DIR/$BASH_ALIAS ~/$BASH_ALIAS
+ln -s $BASE_DIR/$BASH_DIR/$BASH_PRO ~/$BASH_PRO
